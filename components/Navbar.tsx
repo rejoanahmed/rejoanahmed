26cb9f9e4facb659b74187b88aa432f554a0fd4f
rejoanahmed <rejoanahmed8@gmail.com>
2023-01-17 17:29:47 +0800

    some components

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { LinkOff } from '@mui/icons-material'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      Navbar
+      <LinkOff />
+    </nav>
+  )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import theme from '../config/muiTheme'
+import createEmotionCache from '../config/createEmotionCache'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
