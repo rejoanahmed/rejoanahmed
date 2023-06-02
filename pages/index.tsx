21830b78842638e50b30c86b58d70c7113a35d2b
rejoanahmed <rejoanahmed8@gmail.com>
2023-06-02 12:55:54 +0600

    todo mapbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Navbar />
-      <h1 className='text-4xl font-bold underline'>Hello world!</h1>
+    <div className='flex flex-col justify-center items-center h-screen bg-slate-400  text-white'>
+      <Link href='/projects/todoMapbox'>
+        <h1 className='font-bold text-3xl'>TODO MAPBOX</h1>
+      </Link>
     </div>
   )
 }
---
+++ pages/projects/todoMapbox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function TodoMapbox() {
+  return <div>TodoMapbox</div>
+}
+
+export default TodoMapbox
