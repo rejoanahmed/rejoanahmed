a488eeefd5dca98bea0332e24f82dc135b5e898b
rejoanahmed <rejoanahmed8@gmail.com>
2023-12-03 20:29:34 +0800

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1 className='text-4xl font-bold text-center'>
-        Launching soon, stay tuned!
+        Launching soon, stay tuned!, lets be awesome
       </h1>
     </main>
   )
