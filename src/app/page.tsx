cdee0eeb9b9babc38ab17b029c13ee3e5a878e6f
rejoanahmed <rejoanahmed8@gmail.com>
2023-12-10 22:13:10 +0800

    images and intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,37 @@
 import Image from 'next/image'
-import { useState } from 'react'
+
+const SkillClassName = 'text-gray-900 font-semibold'
 
 export default function Home() {
   return (
     <main>
-      <h2 className=''>hey, I&apos;m rejoan 👋</h2>
-      <p className=''>
-        I&apos;m a Part-time freelancer, full-stack maestro.I am obsessed with
-        crafting digital experiences with precision. I love{' '}
-        <Image
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={60}
-          height={10}
-          className='inline-block not-prose mb-1 dark:bg-slate-400 dark:p-1 dark:rounded'
-        />{' '}
-        and{' '}
-        <Image
-          src='/tailwind.svg'
-          alt='Next.js Logo'
-          width={30}
-          height={10}
-          className='inline-block not-prose mb-1'
-        />{' '}
-        tailwindCSS. Let&apos;s turn ideas into code magic.
-      </p>
+      <div className='flex justify-between gap-8 flex-wrap-reverse items-end'>
+        <div className='shrink-0 basis-72 grow p-0'>
+          <h2>hey, I&apos;m rejoan 👋</h2>
+          <p className='text-justify'>
+            I am a full stack developer with a passion for developing quality
+            software. I have a strong passion for social impact and global
+            challenges, and I have developed and deployed robust and scalable
+            web applications using <span className={SkillClassName}>HTML</span>,{' '}
+            <span className={SkillClassName}>CSS</span>,{' '}
+            <span className={SkillClassName}>JavaScrip</span>, and various
+            frameworks, such as <span className={SkillClassName}>React</span>,
+            <span className={SkillClassName}>Next JS</span>,{' '}
+            <span className={SkillClassName}>Flutter</span>, and{' '}
+            <span className={SkillClassName}>React Native</span>.
+          </p>
+        </div>
+
+        <div className='w-60 h-60 shrink-0 grow sm:grow-0 flex justify-center items-center'>
+          <Image
+            src='/me/4.png'
+            width={200}
+            height={200}
+            alt='Rejoan Ahmed'
+            className='rounded-full border-4 border-pink-500 block'
+          />
+        </div>
+      </div>
     </main>
   )
 }
