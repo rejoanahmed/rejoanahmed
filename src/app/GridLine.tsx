05df4cb6cf5413b8b7906eac28cb085f6cf3e9ef
rejoanahmed <rejoanahmed8@gmail.com>
2023-12-27 20:41:16 +0800

    dark

--- src/app/GridLine.tsx
+++ src/app/GridLine.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function GridLine({ className }: { className?: string }) {
   const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  console.log(resolvedTheme, className ?? 'none');
   const cls = resolvedTheme === 'dark' ? 'gridLine-Dark' : 'gridLine-Light';
-  return <div className={cls + ' ' + className}></div>;
+  return <div className={cls + ' ' + className ?? ''}></div>;
 }
 
 export default GridLine;
