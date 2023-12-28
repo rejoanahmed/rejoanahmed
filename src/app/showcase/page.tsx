c6ed8f73f7cc963150332c5424c4c6b1744b5d82
rejoanahmed <rejoanahmed8@gmail.com>
2023-12-28 12:42:50 +0800

    card

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -12,16 +12,22 @@ const itemData = [
   {
     comp: ExclusionTabs,
     title: 'Eclusion Tabs',
-    description: 'animated tabs built with framer motion'
+    description: 'Try clicking on the tabs and see what happens.'
   },
   {
     comp: MacDock,
     title: 'mimicing Mac OS Dock',
-    description: 'animated dock items built with framer motion'
+    description: 'Hover over the icons with a mouse to see the magic.'
+  },
+  {
+    comp: HoverCard,
+    title: 'Radial Background Hover Card',
+    description: 'Hover over the card to see the magic.'
   }
 ];
 
 import ExclusionTabs from './ExclusionTabs';
+import HoverCard from './HoverCard';
 import MacDock from './Macdock';
 import ShowCaseGrid from './ShowcaseGrid';
 
